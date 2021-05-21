e625fa685f0785708c777d173b27950fedc51f88
siyual-park <kdpark0723@gmail.com>
2021-05-22 01:18:58 +0900

    Add broadcast

---
+++ domains/broadcast/lib/index.ts
@@ -0,0 +1,4 @@
+export { default as Session } from "./session";
+export { default as BroadcastStations } from "./broadcast-stations";
+export { default as Event } from "./event";
+
---
+++ domains/broadcast/lib/session.ts
@@ -0,0 +1,5 @@
+interface Session {
+  id: string;
+}
+
+export default Session;
