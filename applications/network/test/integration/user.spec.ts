4754be27b17662c6b8d9c888f73737f124d60e26
siyual-park <kdpark0723@gmail.com>
2021-05-22 18:57:31 +0900

    add test

--- applications/network/test/integration/user.spec.ts
+++ applications/network/test/integration/user.spec.ts
@@ -5,6 +5,7 @@ import { PlainUser } from "@fcs/entity";
 import { SignUpRequest } from "@fcs/auth";
 
 import createRequest from "../create-request";
+import createAuthorization from "../create-authorization";
 
 let request: supertest.SuperTest<supertest.Test>;
 
@@ -37,5 +38,23 @@ describe("POST /users", () => {
     };
     await request.post("/users").send(signUpRequest).expect(201);
     await request.post("/users").send(signUpRequest).expect(409);
+  });
+});
+
+describe("GET /users/self", () => {
+  test("success", async () => {
+    const authorization = await createAuthorization(request);
+
+    const result = await request
+      .get("/users/self")
+      .set("Authorization", authorization)
+      .expect(200);
+
+    const json = camelCase(result.body) as PlainUser;
+    expect(json.id).not.toBeUndefined();
+    expect(json.username).not.toBeUndefined();
+    expect(json.email).not.toBeUndefined();
+    expect(json.createdAt).not.toBeUndefined();
+    expect(json.updatedAt).not.toBeUndefined();
   });
 });
